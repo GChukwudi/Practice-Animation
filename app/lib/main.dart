8c0ef1c0ef231ba19d3dc58d18d21b102e993759
GChukwudi <g.chukwudi@alustudent.com>
2024-10-12 06:49:15 +0200

    updated

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,25 @@
+// simple flutter store app with dark theme toggle
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'theme.dart';
 import 'product.dart';
 
+// Product model
 void main() {
   runApp(MyApp());
 }
 
+// Main app widget
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
+// Main app state
 class _MyAppState extends State<MyApp> {
   bool isDarkTheme = false;
 
+  // List of products
   List<Product> products = [
     Product(
         name: 'Product 1', imageUrl: 'assets/images/product1.jpg', price: 10.0),
@@ -26,16 +31,23 @@ class _MyAppState extends State<MyApp> {
         name: 'Product 4', imageUrl: 'assets/images/product4.jpg', price: 40.0),
   ];
 
+  // Build the app
   @override
   Widget build(BuildContext context) {
+    // Return the app
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Store',
+      // Set the theme based on the isDarkTheme flag
       theme: isDarkTheme ? MyTheme.darkTheme : MyTheme.lightTheme,
+      // Home page
       home: CatalogPage(
         products: products,
+        // Pass the onThemeToggle callback to the CatalogPage
         onThemeToggle: () {
+          // Toggle the theme
           setState(() {
+            // Set the isDarkTheme flag to the opposite value
             isDarkTheme = !isDarkTheme;
           });
         },
@@ -44,10 +56,14 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+// Catalog page
 class CatalogPage extends StatelessWidget {
+  // List of products
   final List<Product> products;
+  // Theme toggle callback
   final VoidCallback onThemeToggle;
 
+  // Constructor
   CatalogPage({required this.products, required this.onThemeToggle});
 
   @override
@@ -57,47 +73,88 @@ class CatalogPage extends StatelessWidget {
         title: Text('Flutter Store'),
         actions: [
           IconButton(
+            // Theme toggle button
             icon: Icon(Icons.brightness_3_rounded),
+            // Call the onThemeToggle callback when the button is pressed
             onPressed: onThemeToggle,
           ),
         ],
-        // titleTextStyle: TextStyle(fontFamily: 'Montserrat', fontSize: 24),
       ),
       body: GridView.builder(
+        // Grid view
         gridDelegate:
+            // 2 columns
             SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemCount: products.length,
         itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text(products[index].name),
-                  content: Text('Price: \$${products[index].price.toString()}'),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('OK'),
-                    ),
-                  ],
-                ),
-              );
-            },
-            child: Card(
-              child: Column(
-                children: [
-                  Image.asset(products[index].imageUrl,
-                      height: 150, width: 150),
-                  Text(products[index].name),
-                  Text('\$${products[index].price.toString()}'),
-                ],
-              ),
-            ),
-          );
+          return ProductCard(product: products[index]);
         },
+      ),
+    );
+  }
+}
+
+// Product card
+class ProductCard extends StatefulWidget {
+  final Product product;
+
+  // Constructor
+  ProductCard({required this.product});
+
+  @override
+  _ProductCardState createState() => _ProductCardState();
+}
+
+// Product card state
+class _ProductCardState extends State<ProductCard> {
+  double opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Delay the animation to simulate fade-in
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        opacity = 1.0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // Show product details when the card is tapped
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(widget.product.name),
+            content: Text('Price: \$${widget.product.price.toString()}'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      },
+      // Product card
+      child: Card(
+        child: Column(
+          children: [
+            AnimatedOpacity(
+              opacity: opacity,
+              duration: Duration(seconds: 1), // Fade-in animation duration
+              child:
+                  Image.asset(widget.product.imageUrl, height: 150, width: 150),
+            ),
+            Text(widget.product.name),
+            Text('\$${widget.product.price.toString()}'),
+          ],
+        ),
       ),
     );
   }
